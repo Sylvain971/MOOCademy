27c4deda22da9dc098cde29cd16c41ca7735a73a
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-02-02 03:30:06 +0100

    push final

--- moocademy/app/models/lesson.rb
+++ moocademy/app/models/lesson.rb
@@ -1,7 +1,8 @@
 class Lesson < ApplicationRecord
 
 	# Guide ruby on rails association basics 2.1 : crée le lien entre les modèles course et lessons
-	belongs_to :course
+	#invers_of clarifie la relation
+	belongs_to :course, inverse_of: course
 	
 	# S'assurer que ces éléments sont bien présents.
 	validates :title, :body, :course,
