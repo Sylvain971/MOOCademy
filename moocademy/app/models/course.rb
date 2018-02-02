435e3219424c68926b8e068668e5ef4a30b02a7a
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-02-02 02:58:24 +0100

    Ajout des règles de validation

--- moocademy/app/models/course.rb
+++ moocademy/app/models/course.rb
@@ -3,4 +3,7 @@ class Course < ApplicationRecord
 	#guide ruby on rails association basics 1 : crée le lien entre les modèles course et lessons
 	has_many :lessons, dependent: :destroy
 
+	# S'assurer que ces éléments sont bien présents.
+	validates :title, :body,
+              presence: true
 end
--- moocademy/app/models/lesson.rb
+++ moocademy/app/models/lesson.rb
@@ -1,6 +1,10 @@
 class Lesson < ApplicationRecord
 
-	#guide ruby on rails association basics 2.1 : crée le lien entre les modèles course et lessons
+	# Guide ruby on rails association basics 2.1 : crée le lien entre les modèles course et lessons
 	belongs_to :course
+	
+	# S'assurer que ces éléments sont bien présents.
+	validates :title, :body,
+              presence: true
 
 end
